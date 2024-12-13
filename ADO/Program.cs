02ddfa25afd9bf64877aaa29e945ce6970fdfb81
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-12-13 21:38:20 +0300

    Add project files.

---
+++ ADO/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Library;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            Console.WriteLine(connectionString);
+            Console.WriteLine("\n---------------------------\n");
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            string cmd = "SELECT * FROM Authors";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+            const int padding = 25;
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.Write(reader.GetName(i).PadRight(padding));
+            }
+            Console.WriteLine();
+
+            if (!reader.IsClosed)
+            {
+                while (reader.Read())
+                {
+                    for (int i = 0;i < reader.FieldCount;i++)
+                    {
+                        Console.Write(reader[i].ToString().PadRight(padding));
+                    }
+                    Console.WriteLine() ;
+                }
+            }
+
+            reader.Close();
+            connection.Close();
+        }
+    }
+}
