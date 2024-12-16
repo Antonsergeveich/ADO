3a3c24ba901f90ad8da92ef9d8faa2273f0d9666
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-12-16 20:50:01 +0300

    +

---
+++ Library2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Library2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Library.Select("author_id,first_name,last_name","Authors");
+		}
+	}
+}
